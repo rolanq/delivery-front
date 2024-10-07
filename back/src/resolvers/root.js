6913f20c3edb1d5eda5474a69d2d428652ebe834
rolanq <rolan.oyun@gmail.com>
2024-10-07 15:06:13 +0300

    changed architecture

--- back/src/resolvers/root.js
+++
@@ -1,9 +0,0 @@
-const { userResolvers } = require("./user");
-const { restuarantsResolvers } = require("./restuarants");
-
-const rootResolvers = {
-  ...userResolvers,
-  ...restuarantsResolvers
-};
-
-module.exports = { rootResolvers };
