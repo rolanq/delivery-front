48c7041464f9ca0a85aabd219e546a1af08ca1cf
rolanq <rolan.oyun@gmail.com>
2024-10-09 15:00:05 +0300

    changed skeleton styles

--- src/features/Restuarant/RestuarantSkeleton/RestuarantSkeleton.tsx
+++ src/features/Restuarant/RestuarantSkeleton/RestuarantSkeleton.tsx
@@ -13,22 +13,16 @@ export const RestuarantSkeleton = () => {
     >
       <CustomSkeleton width="100%" height="260px" />
       <Flex vertical className={styles.content}>
+        <CustomSkeleton borderRadius="12px" width="100%" height="30px" />
         <Flex vertical className={styles.skeletonContent}>
+          <CustomSkeleton width="250px" height="30px" />
           <Flex gap="10px">
-            <CustomSkeleton width="100%" height="150px" />
-            <CustomSkeleton width="100%" height="150px" />
+            <CustomSkeleton borderRadius="24px" width="100%" height="250px" />
+            <CustomSkeleton borderRadius="24px" width="100%" height="250px" />
           </Flex>
           <Flex gap="10px">
-            <CustomSkeleton width="100%" height="150px" />
-            <CustomSkeleton width="100%" height="150px" />
-          </Flex>
-          <Flex gap="10px">
-            <CustomSkeleton width="100%" height="150px" />
-            <CustomSkeleton width="100%" height="150px" />
-          </Flex>
-          <Flex gap="10px">
-            <CustomSkeleton width="100%" height="150px" />
-            <CustomSkeleton width="100%" height="150px" />
+            <CustomSkeleton borderRadius="24px" width="100%" height="250px" />
+            <CustomSkeleton borderRadius="24px" width="100%" height="250px" />
           </Flex>
         </Flex>
       </Flex>
--- src/shared/kit/CustomSkeleton/CustomSkeleton.tsx
+++ src/shared/kit/CustomSkeleton/CustomSkeleton.tsx
@@ -1,18 +1,26 @@
 import { Skeleton } from "antd";
 import { FC } from "react";
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
+import classNames from "classnames";
 
 interface IProps {
   width?: string | number;
   height?: string | number;
+  borderRadius?: string | number;
+  className?: string;
 }
 
-export const CustomSkeleton: FC<IProps> = ({ width = "100%", height = "100%" }) => {
+export const CustomSkeleton: FC<IProps> = ({
+  width = "100%",
+  height = "100%",
+  borderRadius = "10px",
+  className,
+}) => {
   return (
     <Skeleton.Node
       active
-      style={{ width, height, borderRadius: "10px" }}
-      rootClassName={styles.skeleton}
+      style={{ width, height, borderRadius }}
+      rootClassName={classNames(styles.skeleton, className)}
     />
   );
 };
